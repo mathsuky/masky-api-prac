b3cd2fb15fa1299b1f19c154c12e8da33c3e2589
masky5859 <mathsuky2406@gmail.com>
2024-07-19 00:23:54 +0900

    changeData

--- main.go
+++ main.go
@@ -7,24 +7,69 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
-type Fruit struct {
-    Name             string `json:"name"`
-    Description      string `json:"description"`
-    NutritionalValue string `json:"nutritionalValue"`
-    Origin           string `json:"origin"`
+type Pokemon struct {
+	JpName     string
+	EnName     string
+	ImgUrl     string
+	FlavorText string
 }
 
-var dataList = []Fruit{
-    {Name: "Apple", Description: "A sweet, edible fruit produced by an apple tree.", NutritionalValue: "52 kcal per 100g", Origin: "Central Asia"},
-    {Name: "Banana", Description: "An elongated, edible fruit produced by several kinds of large herbaceous flowering plants.", NutritionalValue: "89 kcal per 100g", Origin: "Southeast Asia"},
-    {Name: "Cherry", Description: "A small, round stone fruit that is typically bright or dark red.", NutritionalValue: "50 kcal per 100g", Origin: "Europe and Asia"},
-    {Name: "Date", Description: "The fruit of the date palm, which is a sweet and chewy fruit.", NutritionalValue: "277 kcal per 100g", Origin: "Middle East"},
-    {Name: "Elderberry", Description: "A small, dark-purple fruit that grows in clusters and is known for its medicinal properties.", NutritionalValue: "73 kcal per 100g", Origin: "Europe, Africa, and parts of Asia"},
-    {Name: "Fig", Description: "A soft fruit with a thin skin that can be eaten ripe or dried.", NutritionalValue: "74 kcal per 100g", Origin: "Western Asia"},
-    {Name: "Grape", Description: "A fruit, botanically a berry, of the deciduous woody vines of the flowering plant genus Vitis.", NutritionalValue: "69 kcal per 100g", Origin: "Near East"},
-    {Name: "Honeydew", Description: "A fruit that has a smooth, pale outer skin and sweet, green flesh inside.", NutritionalValue: "36 kcal per 100g", Origin: "West Africa"},
-    {Name: "Indian Fig", Description: "Also known as prickly pear, a species of cactus that produces an edible fruit.", NutritionalValue: "41 kcal per 100g", Origin: "Mexico"},
-    {Name: "Jackfruit", Description: "The largest fruit that grows on a tree, with a distinctive sweet and fruity aroma.", NutritionalValue: "95 kcal per 100g", Origin: "South India"},
+
+var pokedex = []Pokemon{
+    {
+        JpName:     "フシギダネ",
+        EnName:     "Bulbasaur",
+        ImgUrl:     "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png",
+        FlavorText: "うまれたときから　せなかに ふしぎな　タネが　うえてあって からだと　ともに　そだつという。",
+    },
+    {
+        JpName:     "フシギソウ",
+        EnName:     "Ivysaur",
+        ImgUrl:     "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/2.png",
+        FlavorText: "つぼみが　せなかに　ついていて ようぶんを　きゅうしゅうしていくと おおきな　はなが　さくという。",
+    },
+    {
+        JpName:     "フシギバナ",
+        EnName:     "Venusaur",
+        ImgUrl:     "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/3.png",
+        FlavorText: "おおきな　はなびらを　ひろげ たいようの　ひかりを　あびていると からだに　げんきが　みなぎっていく。",
+    },
+    {
+        JpName:     "ヒトカゲ",
+        EnName:     "Charmander",
+        ImgUrl:     "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png",
+        FlavorText: "しっぽの　ほのおは　ヒトカゲの せいめいりょくの　あかし。 げんきだと　さかんに　もえさかる。",
+    },
+    {
+        JpName:     "リザード",
+        EnName:     "Charmeleon",
+        ImgUrl:     "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/5.png",
+        FlavorText: "しっぽを　ふりまわして　あいてを なぎたおし　するどい　ツメで ズタズタに　ひきさいてしまう。",
+    },
+    {
+        JpName:     "リザードン",
+        EnName:     "Charizard",
+        ImgUrl:     "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/6.png",
+        FlavorText: "くちから　しゃくねつの　ほのおを はきだすとき　しっぽの　さきは より　あかく　はげしく　もえあがる。",
+    },
+    {
+        JpName:     "ゼニガメ",
+        EnName:     "Squirtle",
+        ImgUrl:     "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/7.png",
+        FlavorText: "こうらに　とじこもり　みを　まもる。 あいての　すきを　みのがさず みずを　ふきだして　はんげきする。",
+    },
+    {
+        JpName:     "カメール",
+        EnName:     "Wartortle",
+        ImgUrl:     "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/8.png",
+        FlavorText: "ポカンと　あたまを　たたかれるとき こうらに　ひっこんで　よける。でも ちょっとだけ　しっぽが　でているよ。",
+    },
+    {
+        JpName:     "カメックス",
+        EnName:     "Blastoise",
+        ImgUrl:     "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/9.png",
+        FlavorText: "からだが　おもたく　のしかかって あいてを　きぜつさせる。 ピンチの　ときは　カラに　かくれる。",
+    },
 }
 
 func main() {
@@ -46,21 +91,21 @@ func main() {
         return c.String(http.StatusOK, "Welcome to the API server!")
     })
 
-    e.GET("/fruits", func(c echo.Context) error {
+    e.GET("/pokemon", func(c echo.Context) error {
         startIndex, err := strconv.Atoi(c.QueryParam("startIndex"))
         if err != nil {
             startIndex = 0
         }
         endIndex, err := strconv.Atoi(c.QueryParam("endIndex"))
         if err != nil {
-            endIndex = len(dataList) - 1
+            endIndex = len(pokedex) - 1
         }
 
-        if startIndex < 0 || endIndex >= len(dataList) || startIndex > endIndex {
+        if startIndex < 0 || endIndex >= len(pokedex) || startIndex > endIndex {
             return c.JSON(http.StatusBadRequest, map[string]string{"error": "Invalid index range"})
         }
 
-        return c.JSON(http.StatusOK, dataList[startIndex:endIndex+1])
+        return c.JSON(http.StatusOK, pokedex[startIndex:endIndex+1])
     })
 
     e.Logger.Fatal(e.Start(":8080"))
